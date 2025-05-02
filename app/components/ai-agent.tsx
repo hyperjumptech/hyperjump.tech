392cbd14f644476a1316bf8761e0a44c84dca402
Denny Pradipta <denny.aditya.p@gmail.com>
2025-05-02 15:56:08 +0700

    Fallback if there is no AI_AGENT_URL

--- app/components/ai-agent.tsx
+++ app/components/ai-agent.tsx
@@ -9,26 +9,28 @@ import "../styles/ai-agent.css";
 
 export const AIAgent = () => {
   useEffect(() => {
-    createChat({
-      webhookUrl: config.AI_AGENT_URL,
-      initialMessages: [
-        `Hi there! 👋`,
-        `I’m HyperBot — your guide to everything about Hyperjump: our services, success stories, and how we solve real tech problems.`,
-        `To see how we can support you with your tech problems, just say: “Can Hyperjump help me with my tech problem?”`
-      ],
-      showWelcomeScreen: false,
-      // Currently, only EN is supported.
-      i18n: {
-        en: {
-          title: "Hi there! 👋",
-          subtitle: "Start a chat. We're here to help you 24/7.",
-          footer: "",
-          getStarted: "New Conversation",
-          inputPlaceholder: "Type your question...",
-          closeButtonTooltip: "Close"
+    if (config.AI_AGENT_URL) {
+      createChat({
+        webhookUrl: config.AI_AGENT_URL,
+        initialMessages: [
+          `Hi there! 👋`,
+          `I’m HyperBot — your guide to everything about Hyperjump: our services, success stories, and how we solve real tech problems.`,
+          `To see how we can support you with your tech problems, just say: “Can Hyperjump help me with my tech problem?”`
+        ],
+        showWelcomeScreen: false,
+        // Currently, only EN is supported.
+        i18n: {
+          en: {
+            title: "Hi there! 👋",
+            subtitle: "Start a chat. We're here to help you 24/7.",
+            footer: "",
+            getStarted: "New Conversation",
+            inputPlaceholder: "Type your question...",
+            closeButtonTooltip: "Close"
+          }
         }
-      }
-    });
+      });
+    }
   }, []);
 
   return <></>;
--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  AI_AGENT_URL: process.env.NEXT_PUBLIC_AI_AGENT_URL
+  AI_AGENT_URL: process.env.NEXT_PUBLIC_AI_AGENT_URL || ""
 };
 
 export default config;

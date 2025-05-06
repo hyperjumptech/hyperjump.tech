c97c461521d36dc834f26f539267263f7a3503b7
Hari Nugraha <haricnugraha@gmail.com>
2025-05-06 18:45:06 +0700

    feat: change colored logo

--- app/(inferenceai)/inferenceai/components/nav.tsx
+++ app/(inferenceai)/inferenceai/components/nav.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/app/utils/tailwind";
-import { ReactNode, useState } from "react";
+import { useState } from "react";
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -19,106 +19,90 @@ export default function Nav({ lang }: { lang: SupportedLanguage }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <StickyNavigationMain isMenuOpen={isOpen}>
-      {({ shouldBeWhite }) => (
-        <>
-          <NavContainer>
-            <Link href="/inferenceai" className="flex items-center">
-              <InferenceAI isOpen={isOpen} />
-            </Link>
+    <StickyNavigationMain>
+      <div className={cn("w-full px-4 py-5 md:px-8", isOpen && "bg-white")}>
+        <div className="mx-auto flex w-full max-w-7xl flex-wrap items-center justify-between transition-all duration-300 group-data-[scroll='false']:border-none">
+          <Link href="/inferenceai" className="flex items-center">
+            <InferenceAI isOpen={isOpen} />
+          </Link>
 
-            <CenterNavItems>
-              <NavigationMenu className="mx-8 xl:mx-0">
-                <NavigationMenuList className="flex gap-5">
-                  {nav(lang).map((item, idx) => (
-                    <NavigationMenuItem key={idx} className="text-center">
-                      <Link
-                        href={item.href}
-                        className={cn(
-                          shouldBeWhite
-                            ? "text-inferenceai-indigo hover:text-hyperjump-blue"
-                            : "text-white hover:text-hyperjump-blue",
-                          "text-lg font-medium transition-colors xl:text-xl"
-                        )}>
-                        {item.label}
-                      </Link>
-                    </NavigationMenuItem>
-                  ))}
-                </NavigationMenuList>
-              </NavigationMenu>
-            </CenterNavItems>
-
-            <RightNavItems>
-              <LanguagePicker lang={lang} />
-              <HeroCTAButton lang={lang} />
-            </RightNavItems>
-
-            {/* Mobile Toggle */}
-            <div className="flex items-center xl:hidden">
-              <LanguagePicker isOpen={isOpen} lang={lang} />
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                className="ml-3 p-2"
-                aria-label="Toggle menu">
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke={shouldBeWhite ? "black" : "white"}
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg">
-                  {isOpen ? (
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  ) : (
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  )}
-                </svg>
-              </button>
-            </div>
-          </NavContainer>
-
-          {/* Mobile Menu */}
-          {isOpen && (
-            <div className="bg-white shadow-md xl:hidden">
-              <div className="mx-auto flex w-full flex-col space-y-4 px-4 py-5 md:px-8">
-                {nav(lang).map((item, idx) => (
-                  <Link
-                    key={idx}
-                    href={item.href}
-                    className="text-2xl text-inferenceai-indigo hover:text-hyperjump-blue"
-                    onClick={() => setIsOpen(false)}>
-                    {item.label}
-                  </Link>
+          <CenterNavItems>
+            <NavigationMenu className="mx-8 xl:mx-0">
+              <NavigationMenuList className="flex gap-5">
+                {nav(lang).map(({ href, label }) => (
+                  <NavigationMenuItem key={href} className="text-center">
+                    <Link
+                      href={href}
+                      className="text-lg font-medium transition-colors group-[[data-scroll=false]]:text-white group-[[data-scroll=true]]:text-inferenceai-indigo group-[[data-scroll=false]]:hover:text-hyperjump-blue group-[[data-scroll=true]]:hover:text-hyperjump-blue xl:text-xl">
+                      {label}
+                    </Link>
+                  </NavigationMenuItem>
                 ))}
-                <HeroCTAButton lang={lang} />
-              </div>
-            </div>
-          )}
-        </>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </CenterNavItems>
+
+          <RightNavItems>
+            <LanguagePicker lang={lang} />
+            <HeroCTAButton lang={lang} />
+          </RightNavItems>
+
+          {/* Mobile Toggle */}
+          <div className="flex items-center xl:hidden">
+            <LanguagePicker isOpen={isOpen} lang={lang} />
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="ml-3 p-2"
+              aria-label="Toggle menu">
+              <svg
+                className={cn(
+                  "h-6 w-6",
+                  isOpen
+                    ? "stroke-black"
+                    : "stroke-white group-[[data-scroll=true]]:stroke-black"
+                )}
+                fill="none"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                {isOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="bg-white shadow-md xl:hidden">
+          <div className="mx-auto flex w-full flex-col space-y-4 px-4 py-5 md:px-8">
+            {nav(lang).map((item, idx) => (
+              <Link
+                key={idx}
+                href={item.href}
+                className="text-2xl text-inferenceai-indigo hover:text-hyperjump-blue"
+                onClick={() => setIsOpen(false)}>
+                {item.label}
+              </Link>
+            ))}
+            <HeroCTAButton lang={lang} />
+          </div>
+        </div>
       )}
     </StickyNavigationMain>
-  );
-}
-
-export function NavContainer({ children }: { children: ReactNode }) {
-  return (
-    <div className="w-full px-4 py-5 md:px-8">
-      <div
-        className={cn(
-          "mx-auto flex w-full max-w-7xl flex-wrap items-center justify-between transition-all duration-300 group-data-[scroll='false']:border-none"
-        )}>
-        {children}
-      </div>
-    </div>
   );
 }
 
@@ -147,9 +131,8 @@ function InferenceAI({ isOpen }: { isOpen: boolean }) {
         width={187}
         height={32}
         className={cn(
-          "h-8",
-          isOpen && "hidden",
-          "group-data-[scroll='true']:hidden"
+          "h-8 group-data-[scroll='true']:hidden",
+          isOpen && "hidden"
         )}
       />
       <Image
@@ -158,9 +141,8 @@ function InferenceAI({ isOpen }: { isOpen: boolean }) {
         width={187}
         height={32}
         className={cn(
-          "hidden h-8",
-          isOpen && "block",
-          "group-data-[scroll='true']:block"
+          "hidden h-8 group-data-[scroll='true']:block",
+          isOpen && "block"
         )}
       />
     </>

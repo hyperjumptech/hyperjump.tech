9c60afbbf4e14265d62d83bc04c839911b41064f
Denny Pradipta <denny.aditya.p@gmail.com>
2025-05-19 14:40:58 +0700

    Fix formatting

--- hooks/use-media.ts
+++ hooks/use-media.ts
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const getInitialState = (query: string, defaultState?: boolean) => {
   // Prevent a React hydration mismatch when a default value is provided by not defaulting to window.matchMedia(query).matches.
@@ -8,14 +8,14 @@ const getInitialState = (query: string, defaultState?: boolean) => {
     return defaultState;
   }
 
-  if (typeof window !== 'undefined') {
+  if (typeof window !== "undefined") {
     return window.matchMedia(query).matches;
   }
 
   // A default value has not been provided, and you are rendering on the server, warn of a possible hydration mismatch when defaulting to false.
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== "production") {
     console.warn(
-      '`useMedia` When server side rendering, defaultState should be defined to prevent a hydration mismatches.'
+      "`useMedia` When server side rendering, defaultState should be defined to prevent a hydration mismatches."
     );
   }
 
@@ -35,12 +35,12 @@ const useMedia = (query: string, defaultState?: boolean) => {
       setState(!!mql.matches);
     };
 
-    mql.addEventListener('change', onChange);
+    mql.addEventListener("change", onChange);
     setState(mql.matches);
 
     return () => {
       mounted = false;
-      mql.removeEventListener('change', onChange);
+      mql.removeEventListener("change", onChange);
     };
   }, [query]);
 

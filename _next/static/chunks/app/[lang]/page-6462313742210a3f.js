3c4f0e57854cfdf067fee8ecf3d58a212e80658d
dennypradipta <dennypradipta@users.noreply.github.com>
2025-08-15 07:04:06 +0000

    deploy: 2483f7ba941b1eb9e72a91fa14289337dc53e4b6

--- _next/static/chunks/app/[lang]/page-6462313742210a3f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[911],{5316:(e,s,n)=>{Promise.resolve().then(n.bind(n,6072)),Promise.resolve().then(n.bind(n,8596)),Promise.resolve().then(n.bind(n,9708)),Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,3063,23))}},e=>{e.O(0,[105,362,874,277,831,439,140,383,72,596,441,964,358],()=>e(e.s=5316)),_N_E=e.O()}]);

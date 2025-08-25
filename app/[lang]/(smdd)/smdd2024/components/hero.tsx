18d157f2d034b09d1369358ab9388fe9cce07ef2
sinta <sintaherena@gmail.com>
2025-08-25 14:27:22 +0700

    Add: Add services dropdown to navbar

--- app/[lang]/(smdd)/smdd2024/components/hero.tsx
+++ app/[lang]/(smdd)/smdd2024/components/hero.tsx
@@ -3,7 +3,7 @@ import { SupportedLanguage } from "@/locales/.generated/types";
 
 export default function SmddHero({ lang }: { lang: SupportedLanguage }) {
   return (
-    <div className="relative h-full w-full bg-[url('/images/smdd/bg.jpg')] bg-cover bg-center px-4 pt-16">
+    <div className="relative h-full w-full bg-[url('/images/smdd/bg.jpg')] bg-cover bg-center px-4 pt-16 md:pb-5">
       <div className="absolute inset-0 z-10 bg-black opacity-40"></div>
       <div className="relative z-40 container mx-auto my-8 w-full text-white">
         <h1 className="mt-0 mb-8 text-2xl leading-tight font-bold xl:my-4 xl:text-5xl">

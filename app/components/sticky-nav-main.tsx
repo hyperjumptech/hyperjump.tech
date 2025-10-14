a152bf9659f89b48d85511fc1c700639689641e1
Denny Pradipta <denny.aditya.p@gmail.com>
2025-10-14 15:21:09 +0700

    Test: Add test for Case Studies Page (#122)

--- app/components/sticky-nav-main.tsx
+++ app/components/sticky-nav-main.tsx
@@ -8,8 +8,8 @@ export default function StickyNavigationMain({
   children
 }: StickyNavigationProps) {
   return (
-    <nav className="fixed top-0 right-0 left-0 z-50 shadow-xs backdrop-blur-[3px] transition duration-300 group-data-[scroll=false]:bg-transparent group-data-[scroll=true]:bg-white">
+    <header className="fixed top-0 right-0 left-0 z-50 shadow-xs backdrop-blur-[3px] transition duration-300 group-data-[scroll=false]:bg-transparent group-data-[scroll=true]:bg-white">
       {children}
-    </nav>
+    </header>
   );
 }

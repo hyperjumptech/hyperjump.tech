eb8a885b830df775f3bfec8bfbeb3faab8ace219
sintaherena <sintaherena@gmail.com>
2025-11-04 12:50:53 +0700

    Add: add mediaPulse url (#139)

--- app/[lang]/(hyperjump)/products/data.ts
+++ app/[lang]/(hyperjump)/products/data.ts
@@ -19,7 +19,8 @@ export function getCommercialProduct(lang: SupportedLanguage) {
     {
       title: productsCommercialData2Title(lang),
       description: productsCommercialData2Text(lang),
-      image: "/images/products/media-pulse.svg"
+      image: "/images/products/media-pulse.svg",
+      urlLearnMore: "https://mediapulse.hyperjump.tech/"
     },
     {
       title: productsCommercialData1Title(lang),

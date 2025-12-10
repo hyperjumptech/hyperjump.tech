afe175e9c4577eff382745bfb021460917126bed
Hari Nugraha <15191978+haricnugraha@users.noreply.github.com>
2025-12-10 16:26:05 +0700

    feat: hide link (#143)

--- app/[lang]/(hyperjump)/products/data.ts
+++ app/[lang]/(hyperjump)/products/data.ts
@@ -20,15 +20,16 @@ export function getCommercialProduct(lang: SupportedLanguage) {
       title: productsCommercialData2Title(lang),
       description: productsCommercialData2Text(lang),
       image: "/images/products/media-pulse.svg"
+      // urlLearnMore: "https://mediapulse.hyperjump.tech/",
+      // url: "https://mediapulse.hyperjump.tech/"
     },
     {
       title: productsCommercialData1Title(lang),
       description: productsCommercialData1Text(lang),
-      image: "/images/products/startGPT.svg",
-      urlLearnMore: "https://startgpt.hyperjump.tech/",
-      url: "https://startgpt.hyperjump.tech/"
+      image: "/images/products/startGPT.svg"
+      // urlLearnMore: "https://startgpt.hyperjump.tech/",
+      // url: "https://startgpt.hyperjump.tech/"
     },
-
     {
       title: productsCommercialData3Title(lang),
       description: productsCommercialData3Text(lang),
